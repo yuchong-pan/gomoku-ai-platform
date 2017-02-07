f1c47de51855f8a5f688be2ca875efa75fa729d6
yuchong-pan <panyuchong@gmail.com>
2017-02-07 16:35:34 +0800

    Added MySQL database configuration

--- django/gomoku/settings.py
+++ django/gomoku/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'gomoku.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '138.197.112.97',
+        'NAME': 'gomoku',
+        'USER': 'root',
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'OPTIONS': { 'charset': 'utf8' },
     }
 }
 
